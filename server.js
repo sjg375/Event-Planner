54e8f51d29c8a32cd570196e643e848e6685997c
sjg375 <sjessiegreen2000@gmail.com>
2024-08-14 06:11:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 //Server.js
 let express = require("express");
 let app = express();
+app.use(express.json());
 
 let port = 3000;
 let hostname = "localhost";
+
+let Events = {};
+
+app.post("/", (req, res) => {
+  //Should Serve all events
+});
+
+app.post("/api/:eventid", (req, res) => {
+  //Need to decide what is included in the json body
+});
+
+app.get("/api/:eventid", (req, res) => {
+  //Should serve the event
+});
 
 app.listen(port, hostname, () => {
   console.log(`Listening at: http://${hostname}:${port}`);
